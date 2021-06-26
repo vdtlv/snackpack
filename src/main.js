48151dc722c54c394480989bf0f1bac046e89c44
vdtlv <komapcheg@gmail.com>
2021-06-27 00:26:10 +0300

    Revert "update"
    This reverts commit f9f90fafe9070b5c80f30d7a347daca61a22e746.

--- src/main.js
+++ src/main.js
@@ -7,18 +7,4 @@ const app = new App({
 	}
 });
 
-function getRandomInt(max) {
-	return Math.floor(Math.random() * max);
-  }
-  
-function trueMath() {
-	let a = getRandomInt(20);
-	let b = getRandomInt(20);
-	console.log("a: ", a, "b: ", b);
-	if (getRandomInt(2)) 
-	  return (a+b); 
-	  else 
-		return (a-b);
-  }
-
 export default app;
